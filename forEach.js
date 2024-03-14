73452d72a1380d2ed0dea1edc3fc44ad1ca10356
MohammadMasumHossain <126000837+MohammadMasumHossain@users.noreply.github.com>
2024-03-14 12:03:41 +0600

    for each method

---
+++ forEach.js
@@ -0,0 +1,14 @@
+// Array.forEach(function(currentValue,index, arr))
+
+// let arr = ["dhaka", "rajshahi", "coxbazar"]
+
+// arr.forEach(function printval(val){
+//     console.log (val);
+// })
+
+
+let arr=["dhaka","rajshahi", "coxbazar"]
+
+arr.forEach((val)=>{
+    console.log(val);
+})
